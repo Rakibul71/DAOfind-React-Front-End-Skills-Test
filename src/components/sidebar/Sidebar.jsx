75e7288510597c3c6980b0bb3e2956ff739bff23
Rakibul71 <rakibulislam202019@gmail.com>
2021-08-21 22:14:51 +0600

    sidebar commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,12 +4,30 @@ import "./sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <p>Leaderbord</p>
-      <p>Dashboard</p>
-      <p>People</p>
-      <p>Learn</p>
-      <p>Sponsor</p>
-      <p>Earn</p>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-home"></i>
+        <p>Leaderbord</p>
+      </div>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-columns"></i>
+        <p>Dashboard</p>
+      </div>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-users"></i>
+        <p>People</p>
+      </div>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-book-open"></i>
+        <p>Learn</p>
+      </div>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-cart-plus"></i>
+        <p>Sponsor</p>
+      </div>
+      <div className="sidebarFontawesome">
+        <i className="fas fa-dollar-sign"></i>
+        <p>Earn</p>
+      </div>
     </div>
   );
 };
